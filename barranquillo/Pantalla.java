469a6b7478394370ba0d7f21ccbff61d401fa2d4
iDeadtroll <javielproductor@gmail.com>
2023-12-30 04:59:04 +0100

    Estructura Base del Proyecto V1.1 ( Cambio Clase Pantalla V1 )

--- barranquillo/Pantalla.java
+++ barranquillo/Pantalla.java
@@ -3,227 +3,137 @@ package barranquillo;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Pantalla {
-
     public static void main(String[] args) {
-        menuPrincipal();
-    }
+        try (Scanner scanner = new Scanner(System.in)) {
+            int opcionPrincipal = 0;
+            int opcionSubMenu = 0;
+            boolean salir = false;
 
-    private static void limpiarPantalla() {
-        System.out.print("\033[2J\033[H");
-    }
 
-    private static void imprimirCabezera(String titulo, String... opciones) {
-        System.out.print("\033[1;7m " + titulo + " | ");
-        for (String opcion : opciones) {
-            System.out.print(opcion + " ");
-        }
-        System.out.print("| ");
-    }
+            while (!salir) {
+                System.out.println("\t\tMenú principal:");
+                System.out.println("1. Gestión películas");
+                System.out.println("2. Gestión socios");
+                System.out.println("3. Realizar venta");
+                System.out.println("4. Realizar alquiler");
+                System.out.println("5. Realizar devolucion");
+                System.out.println("6. Salir");
 
-    private static char seleccionarOpcion() {
-        Scanner sc = new Scanner(System.in);
-        char c = sc.nextLine().toLowerCase().trim().charAt(0);
-        System.out.print("\033[A\033[2K\033[0m");
-        return c;
-    }
 
-    private static void menuPrincipal() {
-        boolean salir = false;
+                try {
+                    opcionPrincipal = Integer.parseInt(scanner.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: Seleccione una opción válida (1-6)");
+                    continue;
+                }
 
-        limpiarPantalla();
-        while (!salir) {
-            imprimirCabezera("Menú principal",
-                    "\033[4mP\033[24melículas",
-                    "\033[4mS\033[24mocios",
-                    "\033[4mV\033[24menta",
-                    "\033[4mA\033[24mlquiler",
-                    "\033[4mD\033[24mevolución",
-                    "\033[4mC\033[24merrar"
-            );
 
-            switch (seleccionarOpcion()) {
-                case 'p' -> {
-                    submenuPeliculas();
-                }
-                case 's' -> {
-                    submenuSocios();
-                }
-                case 'v' -> {
-                    /* realizar venta */
-                    System.out.println("Realizar venta...\n...\n...");
-                }
-                case 'a' -> {
-                    /* realizar alquiler */
-                    System.out.println("Realizar alquiler...\n...\n...");
-                }
-                case 'd' -> {
-                    /* realizar devolucion */
-                    System.out.println("Realizar devolucion...\n...\n...");
-                }
-                case 'c' -> {
-                    salir = true;
-                }
-                default -> {
-                    manejarOpcionDesconocida();
-                }
-            }
-        }
-        limpiarPantalla();
-    }
+                switch (opcionPrincipal) {
+                    case 1:
+                        opcionSubMenu = 0;
+                        while (opcionSubMenu != 5) {
+                            System.out.println("\t\tGestión películas:");
+                            System.out.println("1. Añadir película");
+                            System.out.println("2. Actualizar datos de película");
+                            System.out.println("3. Consultar película");
+                            System.out.println("4. Listar películas");
+                            System.out.println("5. Salir al menú principal");
 
-    private static void submenuPeliculas() {
-        boolean volver = false;
 
-        limpiarPantalla();
-        while (!volver) {
-            imprimirCabezera("Gestión de películas",
-                    "\033[4mA\033[24mñadir",
-                    "Ac\033[4mt\033[24mualizar",
-                    "\033[4mC\033[24monsultar",
-                    "\033[4mL\033[24mistar",
-                    "\033[4mV\033[24molver"
-            );
+                            try {
+                                opcionSubMenu = Integer.parseInt(scanner.nextLine());
+                            } catch (NumberFormatException e) {
+                                System.out.println("Error: Seleccione una opción válida (1-5)");
+                                continue;
+                            }
 
-            switch (seleccionarOpcion()) {
-                case 'a' -> {
-                    /* añadir película */
-                    System.out.println("Añadir película...\n...\n...");
-                }
-                case 't' -> {
-                    /* actualizar película */
-                    System.out.println("Actualizar película...\n...\n...");
-                }
-                case 'c' -> {
-                    /* consultar película */
-                    System.out.println("Consultar película...\n...\n...");
-                }
-                case 'l' -> {
-                    /* listar películas */
-                    System.out.println("Listar películas...\n...\n...");
-                }
-                case 'v' -> {
-                    volver = true;
-                }
-                default -> {
-                    manejarOpcionDesconocida();
-                }
-            }
-        }
-        limpiarPantalla();
-    }
 
-    private static void submenuSocios() {
-        boolean volver = false;
+                            switch (opcionSubMenu) {
+                                case 1:
+                                    System.out.println("Has seleccionado la opción 1 del submenú gestión películas");
+                                    break;
+                                case 2:
+                                    System.out.println("Has seleccionado la opción 2 del submenú gestión películas");
+                                    break;
+                                case 3:
+                                    System.out.println("Has seleccionado la opción 3 del submenú gestión películas");
+                                    break;
+                                case 4:
+                                    System.out.println("Has seleccionado la opción 4 del submenú gestión películas");
+                                    break;
+                                case 5:
+                                    System.out.println("Volviendo al menú principal...");
+                                    break;
+                                default:
+                                    System.out.println("Seleccione una opción válida (1-5)");
+                                    break;
+                            }
+                        }
+                        break;
+                    case 2:
+                        opcionSubMenu = 0;
+                        while (opcionSubMenu != 5) {
+                            System.out.println("\t\tGestión socios:");
+                            System.out.println("1. Dar de alta a socio");
+                            System.out.println("2. Dar de baja a socio");
+                            System.out.println("3. Consultar datos de socio");
+                            System.out.println("4. Listar socios");
+                            System.out.println("5. Salir al menú principal");
 
-        limpiarPantalla();
-        while (!volver) {
-            imprimirCabezera("Gestión de socios",
-                    "\033[4mA\033[24mlta",
-                    "\033[4mB\033[24maja",
-                    "\033[4mC\033[24monsultar",
-                    "\033[4mL\033[24mistar",
-                    "\033[4mV\033[24molver"
-            );
 
-            switch (seleccionarOpcion()) {
-                case 'a' -> {
-                    /* alta socio */
-                    System.out.println("Alta socio...\n...\n...");
-                }
-                case 'b' -> {
-                    /* baja socio */
-                    System.out.println("Baja socio...\n...\n...");
-                }
-                case 'c' -> {
-                    /* baja socio */
-                    System.out.println("Baja socio...\n...\n...");
-                }
-                case 'l' -> {
-                    /* listar socios */
-                    System.out.println("Listar socios...\n...\n...");
-                }
-                case 'v' -> {
-                    volver = true;
-                }
-                default -> {
-                    manejarOpcionDesconocida();
+                            try {
+                                opcionSubMenu = Integer.parseInt(scanner.nextLine());
+                            } catch (NumberFormatException e) {
+                                System.out.println("Error: Seleccione una opción válida (1-5)");
+                                continue;
+                            }
+
+
+                            switch (opcionSubMenu) {
+                                case 1:
+                                    System.out.println("Has seleccionado la opción 1 del submenú gestión socios");
+                                    break;
+                                case 2:
+                                    System.out.println("Has seleccionado la opción 2 del submenú gestión socios");
+                                    break;
+                                case 3:
+                                    System.out.println("Has seleccionado la opción 3 del submenú gestión socios");
+                                    break;
+                                case 4:
+                                    System.out.println("Has seleccionado la opción 4 del submenú gestión socios");
+                                    break;
+                                case 5:
+                                    System.out.println("Volviendo al menú principal...");
+                                    break;
+                                default:
+                                    System.out.println("Seleccione una opción válida (1-5)");
+                                    break;
+                            }
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Realizar Venta");
+                        break;
+                    case 4:
+                        System.out.println("Realizar Alquiler");
+                        break;
+                    case 5:
+                        System.out.println("Realizar Devolucion");
+                        break;
+                    case 6:
+                        salir = true;
+                        System.out.println("Saliendo...");
+                        break;
+                    default:
+                        System.out.println("Seleccione una opción válida (1-6)");
+                        break;
                 }
             }
         }
-        limpiarPantalla();
     }
 
-    private static void manejarOpcionDesconocida() {
-        System.out.print("\007");
-    }
     
-
-    public void confirmacionDeAlta() {
-        System.out.println("Alta realizada.");
-    }
-
-    public void confirmacionDePelicula() {
-        System.out.println("Película añadida.");
-    }
-
-    public void confirmacionDeBaja() {
-        System.out.println("Baja realizada.");
-    }
-
-    public void imprimePelicula(Pelicula pe) {
-    
-    }
-
-    public void imprimePeliculas(List<Pelicula> lp) {
-        for (Pelicula pelicula : lp) {
-            imprimePelicula(pelicula);
-        }
-    }
-
-    public void imprimeRecibo(Recibo r) {
-    
-    }
-
-    public void imprimeSocio(Socio s) {
-    
-    }
-
-    public void imprimeSocios(List<Socio> ls) {
-        for (Socio socio : ls) {
-            imprimeSocio(socio);
-        }
-    }
-
-    public void resumePelicula(Pelicula pe) {
-    
-    }
-
-    public void resumeSocio(Socio s) {
-    
-    }
-
-    public String pedirNombre() {
-        
-        System.out.println("Introduzca el nombre del socio:");
-        Scanner sc = new Scanner(System.in);
-        String nombre = sc.nextLine();
-        return nombre;
-    }
-
-    public String pedirApellidos() {
-        
-        System.out.println("Introduzca los apellidos del socio:");
-        Scanner sc = new Scanner(System.in);
-        String apellidos = sc.nextLine();
-        return apellidos;
-    }
-
-    public String pedirTelefono() {
-        
-        System.out.println("Introduzca el teléfono del socio:");
-        Scanner sc = new Scanner(System.in);
-        String telefono = sc.nextLine();
-        return telefono;
-    }
 }
+
